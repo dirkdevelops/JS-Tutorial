5898e8ef83e0e9e40f3b6acd58cddecd32cfcd1c
Dirk <noyahrdirk@gmail.com>
2022-05-18 17:50:46 -0400

    Boolean Practice

--- index.js
+++ index.js
@@ -12,29 +12,27 @@ console.log(num * num2)
 
 //Lesson 5.3 Numbers, booleans and conditionals
 const myMoney = 300;
-const myFriendsMoney = 200 + 20; // primitive data type
+const myFriendsMoney = 200 + 20; // number, strings and booleans = primitive data type
 const totalMoney= myMoney + myFriendsMoney;
 console.log(totalMoney);
 
 //remainder/modulo
 const change = totalMoney % 100;
-// % gives you the remainder not the result
+// % gives you the remainder not the result (essentially you want $100 bills with the $20 remaining)
 
 console.log(change);
 
 // booleans
 
-const iHaveMoreMoney = myMoney < myFriendsMoney;
-// making a comparison here, result of expression is a boolean which is a true or false, true or false is always lowercase, can put true or false in a const or let
-// comparions operators > < >= <= === (triple equal to assert equality)
+const iHaveMoreMoney = myMoney > myFriendsMoney;
+// making a comparison here, result of expression is a boolean which is a true or false, true or false is always lowercase and no '', can put true or false in a const or let
+// comparions operators > , < , >= , <= , === (triple equal to assert equality)
 console.log (iHaveMoreMoney);
 
 if (iHaveMoreMoney) {
     console.log('Im rich')
 
 }
-
-
 // if i have more money i use curley brackets to run some code (blockstatement); since iHaveMoreMoney is false then the code will not run if it is true than it will run
 
 if (myMoney < myFriendsMoney){
@@ -50,3 +48,19 @@ else {
 }
 
 // else or else if
+
+// Lesson 5.4 - Numbers and Booleans Practice
+const number = 300;
+
+// print even if my money is even
+// print odd if my money is odd
+
+if (number % 2 === 0){
+    console.log("The number is even.");
+}
+
+else {
+    console.log("The number is odd.");
+}
+
+// even numbers are exactly divisble by 2 with a remainder of 0, so use remainder opeator 
